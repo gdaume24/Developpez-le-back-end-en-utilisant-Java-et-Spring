b0aab15e072448076b0b385d4288b830fb672dde
Geoffroy <gdaumer@2cconseil.com>
2024-10-24 16:55:47 +0200

    La requête quand on lance mockoon comme sur le Dockerfile mais en local fonctionne. A voir pour requêter via le Docker, et ensuite faire les endpoint qui vont interagir avec le docker SQL (ça va être trop facile :'(...)

--- Back/src/main/java/com/rentals/controllers/LoginController.java
+++ Back/src/main/java/com/rentals/controllers/LoginController.java
@@ -1,14 +1,22 @@
 package com.rentals.controllers;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.rentals.dao.ProductDao;
 import com.rentals.models.Product;
 import com.rentals.services.JWTService;
@@ -32,14 +40,28 @@ public class LoginController {
     }
 
     @PostMapping(value = "/Produits")
-    public void ajouterProduit(@RequestBody Product product) {
-    productDao.save(product);
+    public ResponseEntity<Product> ajouterProduit(@RequestBody Product product) {
+        Product productAdded = productDao.save(product);
+        if (Objects.isNull(productAdded)) {
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productAdded.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/Produits")
-    public List<Product> listeProduits() {
-        return productDao.findAll();
-    }
+    public MappingJacksonValue listeProduits() {
+        List<Product> produits = productDao.findAll();
+        SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
+        FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("monFiltreDynamique", monFiltre);
+        MappingJacksonValue produitsFiltres = new MappingJacksonValue(produits);
+        produitsFiltres.setFilters(listDeNosFiltres);
+        return produitsFiltres;
+        }
 
     //Récupérer un produit par son Id
     @GetMapping(value = "/Produits/{id}")
--- Back/src/main/java/com/rentals/models/Product.java
+++ Back/src/main/java/com/rentals/models/Product.java
@@ -1,53 +1,74 @@
 package com.rentals.models;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+    @Id 
+    @GeneratedValue
   private int id;
   private String nom;
   private int prix;
+  // @JsonFilter("monFiltreDynamique")
+  private int prixAchat;
 
   public Product() {
   }
 
-public Product(int id, String nom, int prix) {
+    public Product(int id, String nom, int prix, int prixAchat) {
 
-   this.id = id;
+    this.id = id;
 
-   this.nom = nom;
+    this.nom = nom;
 
-   this.prix = prix;
+    this.prix = prix;
 
-}
+    this.prixAchat = prixAchat;
 
-  public int getId() {
-      return id;
-  }
+    }
 
-  public void setId(int id) {
-      this.id = id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public String getNom() {
-      return nom;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public void setNom(String nom) {
-      this.nom = nom;
-  }
+    public String getNom() {
+        return nom;
+    }
 
-  public int getPrix() {
-      return prix;
-  }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-  public void setPrix(int prix) {
-      this.prix = prix;
-  }
+    public int getPrix() {
+        return prix;
+    }
 
-  @Override
-  public String toString() {
-      return "Product{" +
-              "id=" + id +
-              ", nom='" + nom + '\'' +
-              ", prix=" + prix +
-              '}';
-  }
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public int getPrixAchat() {
+        return prixAchat;
+    }
+
+    public void setPrixAchat(int prixAchat) {
+        this.prixAchat = prixAchat;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                '}';
+    }
 }
